8c545713bdf5377bf33b0d675f0679a6a67ceb91
Luke Barousse <luke.barousse@gmail.com>
2022-11-10 13:52:12 -0600

    Update footer all pages

--- pages/04_🌎_Explore.py
+++ pages/04_🌎_Explore.py
@@ -100,3 +100,58 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 st.dataframe(filter_dataframe(jobs_all))
+
+##########
+# Footer #                         #  https://discuss.streamlit.io/t/st-footer/6447
+##########
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
+
+def image(src_as_string, **style):
+    return img(src=src_as_string, style=styles(**style))
+
+def link(link, text, **style):
+    return a(_href=link, _target="_blank", style=styles(**style))(text)
+
+def layout(*args):
+    style = """
+    <style>
+        MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        .stApp { bottom: 0px; }
+    </style>
+    """
+
+    style_div = styles(
+        position="fixed",
+        right=0,
+        bottom=0,
+        margin=px(0, 100, 0, 0),
+        text_align="center",
+        opacity=1,
+    )
+
+    body = p()
+    foot = div(
+        style=style_div
+    )(
+        body
+    )
+
+    st.markdown(style, unsafe_allow_html=True)
+    for arg in args:
+        if isinstance(arg, str):
+            body(arg)
+        elif isinstance(arg, HtmlElement):
+            body(arg)
+    st.markdown(str(foot), unsafe_allow_html=True)
+
+def footer():
+    myargs = [
+        link("https://serpapi.com/", image('https://github.com/lukebarousse/Data_Analyst_Streamlit_App_V1/raw/main/images/SerpAPI_v1.png',)),
+    ]
+    layout(*myargs)
+
+if __name__ == "__main__":
+    footer()
